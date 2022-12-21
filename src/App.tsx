0a9767fc27ad11c6cdeb3ee8209456e0b54c85c2
Eduardo <eduardo.rocha2502@gmail.com>
2022-12-21 14:12:39 -0300

    area welcome criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './components/header';
+
+const avatar = require("./assets/images/avatar.png") as string;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='welcome-area'>
+        <div className='container'>
+          <Header />
+          <div className='welcome-area--perfil'>
+            <div className='welcome-area--avatar'>
+              <img src={avatar} alt='avatar'/>
+            </div>
+            <h2>Hi! I'm Eduardo <br /> Web Developer</h2>
+            <div className='welcome-div'>
+              Welcome to my Portfolio
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,29 @@
+import './header.css';
+
+const linkedinIcon = require("../assets/images/linkedin_icon.png") as string;
+const gitHubIcon = require("../assets/images/github_icon.png") as string;
+
+const Header = () => {
+    return (
+        <header>
+            <div className="logo">LOGO</div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Skills</li>
+                    <li>Projects</li>
+                </ul>
+            </nav>
+            <div className='social'>
+                <a href='https://www.linkedin.com/in/eduardo-rocha-553486212/' target='_blank' className='social-icon'>
+                    <img src={linkedinIcon} alt='linkedin-icon' />
+                </a>
+                <a href='https://github.com/eduardorocham' target='_blank' className='social-icon'>
+                    <img src={gitHubIcon} alt='github-icon' />
+                </a>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
