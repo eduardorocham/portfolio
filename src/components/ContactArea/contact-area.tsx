e7564d9a6ae2e7b7c88b00ee2518c5a83286a675
Eduardo <eduardo.rocha2502@gmail.com>
2023-01-20 14:06:30 -0300

    send email

--- src/components/ContactArea/contact-area.tsx
+++ src/components/ContactArea/contact-area.tsx
@@ -1,6 +1,7 @@
 import './contact-area.css';
 import { useState } from 'react';
 import { Container } from '../container';
+import emailjs from '@emailjs/browser';
 
 const AstronautaImage = require("../../assets/images/astronauta.png") as string;
 
@@ -10,6 +11,32 @@ const ContactArea = () => {
     const [subject, setSubject] = useState('');
     const [message, setMessage] = useState('');
 
+    const templateParams = {
+        from_name: name,
+        message: message,
+        email: email
+    }
+
+    const sendEmail = (e : any) => {
+        e.preventDefault();
+
+        if (email === '' || name === '' || subject === '' || message === '') {
+            alert('All fields are required');
+            return
+        }
+
+        emailjs.send("service_mo0ogsg", "template_u3zvgap", templateParams, "812tj0566nGOA074L")
+        .then((res) => {
+            console.log('EMAIL ENVIADO!', res.status, res.text);
+            setName('');
+            setEmail('');
+            setSubject('');
+            setMessage('');
+        }, (error) => {
+            console.log('ERRO', error);
+        });
+    };
+
     return (
         <div className="contact-area">
             <Container>
@@ -17,7 +44,7 @@ const ContactArea = () => {
                     <div className='contact-area-left'>
                         <h1>Get in Touch</h1>
 
-                        <form>
+                        <form onSubmit={sendEmail}>
                             <fieldset>
                                 <div className='campo'>
                                     <input 
